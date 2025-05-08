09b9042e45d0df843f0d2c5b68b0550dd0a05391
malaprince11 <60395456+malaprince11@users.noreply.github.com>
2025-05-08 18:49:07 +0200

    feat: add gradient bg for team card

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Player } from "../../types/types";
 
 interface CardProps {
   id: number;
@@ -20,12 +19,17 @@ const Card: React.FC<CardProps> = ({
   division,
   full_name,
 }) => {
+  
+  const gradientClass = conference === "West" 
+    ? "bg-gradient-to-br from-red-500 to-red-700" 
+    : "bg-gradient-to-br from-blue-500 to-indigo-600";
+
   return (
     <Link 
       className="p-8 max-w-lg border border-grey-300 rounded-2xl hover:shadow-xl hover:shadow-indigo-50 flex flex-col items-center justify-center"
       to={`/team/${id}`}
     >
-      <div className="w-32 h-32 bg-gradient-to-br from-blue-500 to-indigo-600 rounded-full flex items-center justify-center shadow-lg">
+      <div className={`w-32 h-32 ${gradientClass} rounded-full flex items-center justify-center shadow-lg`}>
         <span className="text-4xl font-bold text-white">{abbreviation}</span>
       </div>
       <div className="mt-8 text-center">
